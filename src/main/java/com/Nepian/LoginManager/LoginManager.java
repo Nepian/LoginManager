562ddd6ebeee83dd63c6df818ba730d9baa22e6b
Nepian <nepia.ms@gmail.com>
2015-11-07 03:06:19 +0900

    no message

--- src/main/java/com/Nepian/LoginManager/LoginManager.java
+++ src/main/java/com/Nepian/LoginManager/LoginManager.java
@@ -1,19 +1,35 @@
 package com.Nepian.LoginManager;
 
+import java.io.File;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LoginManager extends JavaPlugin {
-	public static LoginManager plugin;
+	private static LoginManager plugin;
+	private static File dataFolder;
+	private static File userDataFolder;
 
 	public LoginManager() {
+		dataFolder = getDataFolder();
 		plugin = this;
 	}
 
 	public void onEnable() {
-
+		userDataFolder = new File(dataFolder, "userdata");
+		if (userDataFolder.exists()) {
+			userDataFolder.mkdirs();
+		}
 	}
 
 	public void onDisable() {
 
+	}
+
+	public static LoginManager getPlugin() {
+		return plugin;
+	}
+
+	public static File getPlayerDataFolder() {
+		return userDataFolder;
 	}
 }
